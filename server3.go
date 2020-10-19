338c7a50179cddceb401a9ae1848fa4aa22b991b
Sean McCullough <s.mccullough@arkoselabs.com>
2020-10-20 08:43:13 +1000

    Add healthcheck api

--- server3.go
+++ server3.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -12,6 +13,11 @@ import (
 const requestStartRequestKey = "requestStart"
 const httpPort = 8000
 
+// Health represents the current state of the running service, and its readiness to accept requests.
+type Health struct {
+	Healthy bool
+}
+
 func init() {
 	// log.SetFormatter(&log.JSONFormatter{})
 	log.SetOutput(os.Stdout)
@@ -20,8 +26,8 @@ func init() {
 
 func main() {
 	mux := http.NewServeMux()
-	finalHandler := http.HandlerFunc(handler)
-	mux.Handle("/", middleware.RequestLoggerMiddleware(finalHandler))
+	mux.Handle("/", middleware.RequestLoggerMiddleware(http.HandlerFunc(handler)))
+	mux.Handle("/health", middleware.RequestLoggerMiddleware(http.HandlerFunc(healthcheckHandler)))
 	log.Infof("listening on %d", httpPort)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", httpPort), mux))
 }
@@ -38,4 +44,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	for k, v := range r.Form {
 		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
 	}
+}
+
+func healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	health := Health{Healthy: true}
+	writeJSON(w, health)
+}
+
+func writeJSON(writer http.ResponseWriter, payload interface{}) {
+	writer.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(payload)
 }
